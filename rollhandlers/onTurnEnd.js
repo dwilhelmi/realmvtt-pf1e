e55ce7f9ed4e70faf38fc9dbbf138d3414f25560
Sean Schnell <seansps@gmail.com>
2025-10-22 17:06:17 -0400

    Add handling for shield lowering on turn end for NPCs

--- rollhandlers/onTurnEnd.js
+++ rollhandlers/onTurnEnd.js
@@ -44,16 +44,23 @@ if (modifiers.length > 0) {
 // Check if the target's shield is up and is a character
 const shieldUp = token?.data?.shieldRaised === "true";
 const isCharacter = token?.recordType === "characters";
-if (shieldUp && isCharacter && token?.recordId) {
-  api.getRecord("characters", token?.recordId, (record) => {
-    if (record) {
-      const valuesToSet = {
-        "data.shieldRaised": "false",
-      };
-      api.setValuesOnRecord(record, valuesToSet, (updatedRecord) => {
-        // If update was successful, then recalc AC and  bonuses
-        onAddEditFeature(updatedRecord, undefined, true);
-      });
-    }
-  });
+if (shieldUp && token?.recordId) {
+  if (isCharacter) {
+    api.getRecord("characters", token?.recordId, (record) => {
+      if (record) {
+        const valuesToSet = {
+          "data.shieldRaised": "false",
+        };
+        api.setValuesOnRecord(record, valuesToSet, (updatedRecord) => {
+          // If update was successful, then recalc AC and  bonuses
+          onAddEditFeature(updatedRecord, undefined, true);
+        });
+      }
+    });
+  } else if (token?.recordType === "npcs" && token?.recordId) {
+    api.setValuesOnTokenById(token?._id, token?.recordType, {
+      "data.shieldRaised": "false",
+      "data.ac": token?.data?.ac - 2,
+    });
+  }
 }
